09fb37d427b00b300852eeaebaf85b5c8f5322c9
Krishna Gupta <up.krishnagupta@gmail.com>
2026-09-08 18:15:48 +0530

    completed zod schema for task

---
+++ services/task-service/src/schemas/task.schema.ts
@@ -0,0 +1,28 @@
+import { z } from 'zod'
+import { TaskPriority, TaskStatus } from '../models/task.model.js'
+
+export const SubtaskSchema = z.object({
+    title: z.string().trim().min(1, "Minimum 1 char is required"),
+    isCompleted: z.boolean().default(false)
+})
+
+export const CreateTaskSchema = z.object({
+    title: z
+        .string()
+        .trim()
+        .min(1, 'Title cannot be empty')
+        .max(120, 'Title cannot exceed 120 characters'),
+    description: z.string().trim().default(''),
+    status: z.enum(TaskStatus).default(TaskStatus.TO_DO),
+    priority: z.enum(TaskPriority).default(TaskPriority.MEDIUM),
+    dueDate: z.coerce.date().nullable().optional().default(null),
+    tags: z.array(z.string().trim().min(1)).optional().default([]),
+    subtasks: z.array(SubtaskSchema).optional().default([]),
+    metadata: z.record(z.string(), z.unknown())
+
+})
+
+export const UpdateTaskSchema = CreateTaskSchema.partial();
+
+export type CreateTaskInput = z.infer<typeof CreateTaskSchema>
+export type UpdateTaskInput = z.infer<typeof UpdateTaskSchema>
